c52943b3e25a73ab50a6eed9503d373fd23e4bbb
igornascimento <igor.nascimento@objectedge.com>
2019-02-12 11:01:28 -0200

    Data binding events with $event (and Typescript casting element)

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created.';
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created succesfuly!';
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
